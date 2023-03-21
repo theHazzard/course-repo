184769519f81467cd4c4c9ffd441290d46e4fab3
Enzo Luis Strongoli <thehazzard@gmail.com>
2023-03-20 22:09:40 -0300

    [feat] receives 1 and returns "1"

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,6 @@
 export function fizzBuzz(number: number) {
+  if (number === 1) {
+    return "1";
+  }
   return "test";
 }
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,4 +4,8 @@ describe("fizzbuzz", () => {
   it("returns a string", () => {
     expect(typeof fizzBuzz(3)).toBe("string");
   });
+
+  it("returns the same number as a string", () => {
+    expect(fizzBuzz(1)).toBe("1");
+  });
 });
